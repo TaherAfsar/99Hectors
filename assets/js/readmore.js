b39914b1637121ae56de0e838fcdfaf02d6a8013
aary69 <aaryhumane2004@gmail.com>
2022-07-26 20:02:26 +0530

    Read more btn

--- assets/js/readmore.js
+++
@@ -1,6 +0,0 @@
-const readMoreBtn = document.querySelector('.btn');
-const text = document.querySelector('.text');
-
-readMoreBtn.addEventListener('click',(e)=>{
-    text.classList.toggle('show-more');
-})
